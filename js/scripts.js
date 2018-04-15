8f5ddef5e665d6df2f446cb53f05229436e3c18a
unknown <swm1000@hotmail.com>
2018-04-14 22:15:51 -0400

    insert playlist data into page

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,23 @@ var audio;
 
 $("#pausebtn").hide();
 
-function getsong(song) {
-    var file = element.attr("file");
-    var title = element.text();
-    var image = element.attr("image");
-    var artist = element.attr("artist");
+getsong($("#playlist li:first-child"));
 
-    song = new Audio("aud/" + file);
+function getsong(song) {
+    var file = song.attr("file");
+    var title = song.text();
+    var image = song.attr("image");
+    var artist = song.attr("artist");
+
+    audioobj = new Audio("aud/" + file);
+    
+    if(!audioobj.currentTime) {
+        $("#progresslength").html("0.00");
+    }
+    $("#audioinfo .title").text(title);
+    $("#audioinfo .artist").text(artist);
+    $("img.songimage").attr("src","img/" + image);
+
+    $("#playlist li").removeClass("active");
+    song.addClass("active");
 }
