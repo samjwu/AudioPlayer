81506fa36da409da11b5a4cb2b9529b3b42bb8b4
unknown <swm1000@hotmail.com>
2018-04-14 22:03:58 -0400

    get song attributes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 var audio;
 
 $("#pausebtn").hide();
+
+function getsong(song) {
+    var file = element.attr("file");
+    var title = element.text();
+    var image = element.attr("image");
+    var artist = element.attr("artist");
+
+    song = new Audio("aud/" + file);
+}
